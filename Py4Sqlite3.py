15b902ebbc0f7f309b8db5f7aa1f13ad498c07b2
soiqualang <soiqualang.chentreu@gmail.com>
2020-02-18 09:59:31 +0700

    t1

---
+++ Py4Sqlite3.py
@@ -0,0 +1,52 @@
+#functions by soiqualang
+
+import sqlite3
+
+def insert_table(table,field,value,conn):
+    with conn:
+        cur = conn.cursor()
+        strfield=""
+        strvalue=""
+        for i in range(0,len(field)):
+            strfield+=field[i]+", "
+            strvalue+="'"+value[i]+"', "
+        strfield+=field[i]
+        strvalue+="'"+value[i]+"'"
+        sql_add_news="INSERT INTO "+table+"("+strfield+") VALUES ("+strvalue+")"
+        #print(sql_add_news)
+        cur.execute(sql_add_news)
+        cur.close()
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+        
+def getElement(tbl_table,element,where,id,conn):
+    with conn:
+        cur = conn.cursor()
+        id=str(id)
+        #sql='SELECT SQLITE_VERSION()'
+        sql="Select "+element+" from "+tbl_table+" where "+where+"='"+id+"'"
+        #print(sql)
+        cur.execute(sql)
+        data = cur.fetchone()
+        #data=cur.fetchall()
+        return data[element]
+        cur.close()
+
+def table_to_array1(table,conn):
+    with conn:
+        conn.row_factory = dict_factory
+        cur = conn.cursor()
+        sql="SELECT * from "+table
+        #print(sql)
+        cur.execute(sql)
+        rows = cur.fetchall()        
+        '''
+        for row in rows:
+            print ("%s %s %s" % (row["id"], row["ques"], row["ans"]))
+        '''
+        return rows
+        cur.close()
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Py4Sqlite3
-Some python function for SQlite3 by soiqualang =))
+Some python functions for SQlite3 by soiqualang =))
+
+> Insert record
+
+```python
+field=['ques','ans','info']
+value=['qqqq2','aaaaa2','iiiii2']
+
+insert_table('qa',field,value,conn)
+```
+
+> Select record
+
+```python
+ans=getElement('qa','ans','id',2,conn)
+print(ans)
+
+# aaaaa2
+```
+
+> Get all table
+
+```python
+rows=table_to_array1('qa',conn)
+#print(rows)
+
+for row in rows:
+    print(row['id'],row['ques'],row['ans'])
+
+# 1 qqqq2 aaaaa2
+# 2 qqqq1 aaaaa1
+```
